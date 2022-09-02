dff3bc3590987fd552658243c3c668dd92e41e8c
julianpulido2003 <julianpulido2014@gmail.com>
2022-09-02 11:27:20 -0500

    Se aceptaron los cambios

--- src/co/edu/unisabana/example/Principal.java
+++ src/co/edu/unisabana/example/Principal.java
@@ -3,12 +3,9 @@ package co.edu.unisabana.example;
 public class Principal {
     public static void main(String[] args) {  
         System.out.println("Hola mundo");
-<<<<<<< HEAD
 
         radicacion.rad();
-=======
         System.out.println(suma.doSuma(0, 0));
->>>>>>> 23a44ed8168702988856acd2217f4ee276577bd7
         System.out.println(Resta.rest());
         System.out.println(multiplicacion.mult());
 
