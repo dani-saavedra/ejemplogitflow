a7832e5354af6fa9df8cda3fa979af28d0f02cc7
Daniela Cortés <dcortes11111@gmail.com>
2022-09-02 10:37:38 -0500

    Sube operacion division

--- src/co/edu/unisabana/example/Principal.java
+++ src/co/edu/unisabana/example/Principal.java
@@ -3,5 +3,6 @@ package co.edu.unisabana.example;
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        new division();
     }
 }
