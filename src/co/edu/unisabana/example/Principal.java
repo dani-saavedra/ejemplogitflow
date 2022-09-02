16a661b8e12ffce358f73bf68744dac923637a67
kareenlama <kareenlatorre03@gmail.com>
2022-09-02 11:06:57 -0500

    Sube operacion multiplicacion

--- src/co/edu/unisabana/example/Principal.java
+++ src/co/edu/unisabana/example/Principal.java
@@ -1,7 +1,11 @@
 package co.edu.unisabana.example;
+
+import co.edu.unisabana.example.multiplicacion;
+
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         new Suma();
+        new multiplicacion();
     }
 }
