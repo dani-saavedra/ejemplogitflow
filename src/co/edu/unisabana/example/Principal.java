bdb236324b3716cb760e443f0bc08f4633fa7f3b
julianpulido2003 <julianpulido2014@gmail.com>
2022-09-02 11:23:25 -0500

    Funcion radicacion

--- src/co/edu/unisabana/example/Principal.java
+++ src/co/edu/unisabana/example/Principal.java
@@ -3,5 +3,8 @@ package co.edu.unisabana.example;
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+
+        radicacion.rad();
+
     }
 }
