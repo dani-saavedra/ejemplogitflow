d1d8354691fafecbf5c616b588f32880ecd951ce
z0ed <diegolmedosz@gmail.com>
2022-09-02 11:48:34 -0500

    Merge with develop

--- src/co/edu/unisabana/example/Principal.java
+++ src/co/edu/unisabana/example/Principal.java
@@ -1,18 +1,12 @@
 package co.edu.unisabana.example;
 
 public class Principal {
-<<<<<<< HEAD
     public static void main(String[] args) {
         System.out.print(Division.doDivision());  
-=======
-    public static void main(String[] args) {  
-        System.out.println("Hola mundo");
-
         radicacion.rad();
         System.out.println(suma.doSuma(0, 0));
         System.out.println(Resta.rest());
         System.out.println(multiplicacion.mult());
 
->>>>>>> 23a44ed8168702988856acd2217f4ee276577bd7
     }
 }
